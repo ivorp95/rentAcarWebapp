cb770a811517850ffe72f59e461c3d3da5566200
ivorp95 <ivpan@proton.me>
2024-01-09 16:58:06 +0100

    modified:   html/index.html 	modified:   html/ponude.php 	modified:   html/registracija.html 	deleted:    html/sve_ponude.html 	new file:   slike/2022-Maserati-MC20.png 	deleted:    slike/5e5f0f05f9703fa3db0cc93b5d2c8578.png 	deleted:    slike/6-5.png 	deleted:    slike/80e2599b680c6eab603049c564896568.jpg 	deleted:    slike/A160053_web_2880.jpg 	deleted:    slike/Aston-Martin-Logo.jpg 	deleted:    slike/Lamborghini_Logo.svg.png 	deleted:    slike/Logo-Tesla.jpg 	deleted:    slike/McLaren-Logo-1998.png 	deleted:    slike/Pagani-Logo.png 	new file:   slike/bentley-continental.jpg 	new file:   slike/bugatti.jpg 	deleted:    slike/fed182ad4607d499a0e4da5fb17cba951753_icon.png 	deleted:    slike/kisspng-porsche-logo-porsche-design-vector-5b566ca09f5f85.7178602915323905606528.jpg 	new file:   slike/opis.txt

--- html/ponude.php
+++ html/ponude.php
@@ -3,19 +3,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rent-a-car </title>
+    <title>Rent-a-caR</title>
     <link rel="stylesheet" href="../CSS/style_web-shop.css"-->
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"-->
 </head>
 <body>
-    <div class="w3-container w3-teal">
+<div class="w3-container w3-teal">
         <nav class="navigacija">
             <ul class="w3-bar w3-black">
-                <a href="./index.html" class="w3-bar-item w3-button">Pocetna stranica</a></li>
-                <a href="http://localhost:2222/PHP/ponude.php" class="w3-bar-item w3-button">ponude</a></li>
-                <a href="./registracija.html" class="w3-bar-item w3-button">Registracija</a></li>
+                <a href="http://127.0.0.1:5501/html/index.html" class="w3-bar-item w3-button">Pocetna stranica</a></li>
+                <a href="http://localhost:2222/ponude.php" class="w3-bar-item w3-button">Sve ponude</a></li>
+                <a href="http://127.0.0.1:5501/html/registracija.html" class="w3-bar-item w3-button">Registracija</a></li>
             </ul>
         </nav>
     </div>
