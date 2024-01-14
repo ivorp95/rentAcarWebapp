cdf17bf2e64cd925a2f8077d2336028e30e5343a
ivorp95 <ivpan@proton.me>
2024-01-14 01:48:16 +0100

    new file:   PHP/racun.php

---
+++ PHP/racun.php
@@ -0,0 +1,70 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rent-a-caR</title>
+    <link rel="stylesheet" href="http://127.0.0.1:5501/CSS/style_web-shop.css"-->
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"-->
+</head>
+<body>
+<div class="w3-container w3-teal">
+        <nav class="navigacija">
+            <ul class="w3-bar w3-black">
+                <a href="http://127.0.0.1:5501/html/index.html" class="w3-bar-item w3-button">Pocetna stranica</a></li>
+                <a href="http://localhost:2222/ponude.php" class="w3-bar-item w3-button">Sve ponude</a></li>
+                <a href="http://127.0.0.1:5501/html/registracija.html" class="w3-bar-item w3-button">Registracija</a></li>
+                <!--a href="http://localhost:2222/najam.php" class="w3-bar-item w3-button">Najam</a></li-->
+            </ul>
+        </nav>
+    </div>
+    <center>
+    <h1>RENT A CAR</h1>
+    <p></p>
+    <?php
+        $server = "ucka.veleri.hr:3306";
+        $database = "ipangos";
+        $username = "ipangos";
+        $password = "11";
+    
+        $conn = mysqli_connect($server, $username, $password, $database);
+        $query = "SELECT * FROM racunRentPZI LEFT OUTER JOIN voziloRentPZI on voziloRentPZI.ID_vozila=racunRentPZI.ID_vozila WHERE ime=?;"+$ime;
+        $res = mysqli_query($conn, $query);
+
+        $ime=$_GET['ime'];
+    ?>
+    </center>
+    <div>ISPIS RACUNA
+    <table border="2px">
+            
+            <th>Proizvodjac</th>
+            <th>Model</th>
+            <th>Godiste</th>
+            <th>Cijena po danu</th>
+            <th>Tip Goriva</th>
+            <th>Slika</th>
+            <th>Opis</th>
+            <th></th>
+            </tr>
+        <?php
+            while($row = mysqli_fetch_assoc($res)){
+                echo "<tr>";
+                echo "<td>".$row ["proizvodac"]."</td>";
+                echo "<td>".$row ["model"]."</td>";
+                echo "<td>".$row ["godiste"]."</td>";
+                echo "<td>".$row ["cijenaPodanu"]."</td>";
+                echo "<td>".$row ["tipGoriva"]."</td>";
+                echo "<td><img src='".$row["slika"]."' width='300px' alt='" ."'></td>";
+                echo "<td>".$row ["opis"]."</td>";
+                echo "<td><a href='http://localhost:2222/najam.php?idVozilo=".$row ["ID_vozila"]."' target='_self'>NAJAM</a></td>";
+                echo "</tr>";
+            }mysqli_close($conn);
+        ?>
+    </div>
+    
+</body>
+</html>
+
+<!--a href="http://localhost:2222/najam.php" target="_self">Vise</a-->
