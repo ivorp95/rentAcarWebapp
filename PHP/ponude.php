d69899b8b6542a9c030b40e353136dd0016df80a
ivorp95 <ivpan@proton.me>
2024-11-26 18:41:21 +0100

    modified:   PHP/ponude.php

--- PHP/ponude.php
+++ PHP/ponude.php
@@ -11,16 +11,16 @@
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rent-a-caR</title>
-    <link rel="stylesheet" href="http://../CSS/style_web-shop.css"-->
+    <link rel="stylesheet" href="../CSS/style_web-shop.css"-->
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"-->
 </head>
 <body>
 <div class="w3-container w3-teal">
         <nav class="navigacija">
             <ul class="w3-bar w3-black">
-                <a href="http://../html/index.html" class="w3-bar-item w3-button">Pocetna stranica</a></li>
-                <a href="http://../ponude.php" class="w3-bar-item w3-button">Sve ponude</a></li>
-                <a href="http://../html/registracija.html" class="w3-bar-item w3-button">Registracija</a></li>
+                <a href="../html/index.html" class="w3-bar-item w3-button">Pocetna stranica</a></li>
+                <a href="../ponude.php" class="w3-bar-item w3-button">Sve ponude</a></li>
+                <a href="../html/registracija.html" class="w3-bar-item w3-button">Registracija</a></li>
             </ul>
         </nav>
     </div>
